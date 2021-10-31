8c9087496dfc6e0c09aabe132dc173a74bb464c8
Dimitar Filipov <grinndev@protonmail.com>
2021-10-31 17:18:58 +0200

    add cors middleware

--- rest-api/server.js
+++ rest-api/server.js
@@ -5,7 +5,11 @@ require('./mongoose');
 const PORT = 3500;
 
 const app = express();
+const cors = require('cors');
 
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 app.use(express.json());
 app.use(routes);
 

d080c3b4e3de6acaf00cb1810528994ce09f9125
Dimitar Filipov <grinndev@protonmail.com>
2021-11-04 23:29:26 +0200

    add Create Booking button to Booking List

--- front-end/src/components/core/BookingsList/BookingsList.jsx
+++ front-end/src/components/core/BookingsList/BookingsList.jsx
@@ -1,3 +1,6 @@
+// React, Hooks
+import { Link } from 'react-router-dom';
+
 //CSS
 import styles from './BookingsList.module.css';
 
@@ -5,7 +8,7 @@ import styles from './BookingsList.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowCircleUp } from '@fortawesome/free-solid-svg-icons'
 
-function BookingsList({ children, changePage, isEnd }) {
+function BookingsList({ children, isEnd }) {
     const handdleGoUp = () => {
         window.scrollTo({
             top: 0,
@@ -20,6 +23,14 @@ function BookingsList({ children, changePage, isEnd }) {
             </ul>
 
             <FontAwesomeIcon onClick={handdleGoUp} className={styles.goUpBtn} icon={faArrowCircleUp}></FontAwesomeIcon>
+
+            {isEnd &&
+                <Link to="/create-booking">
+                    <button className={styles.createBookingBtn}>
+                        NO MORE BOOKINGS. CREATE A A NEW ONE!
+                    </button>
+                </Link>
+            }
         </>
     )
 }

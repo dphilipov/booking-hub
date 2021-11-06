347303c231533b57bfe95e2fab3d89070a9a92d7
Dimitar Filipov <grinndev@protonmail.com>
2021-11-06 21:55:10 +0200

    add more validations

--- front-end/src/services/validationServices.js
+++ front-end/src/services/validationServices.js
@@ -1,12 +1,18 @@
 function validate(input) {
+    const regex = /^([ \u00c0-\u01ffa-zA-Z'-])+$/;
+
     let errors = {};
 
     if (!input.firstName.trim()) {
         errors.firstName = "First Name can't be empty!";
+    } else if (!regex.test(input.firstName.trim())) {
+        errors.firstName = "Invalid format!";
     }
 
     if (!input.lastName.trim()) {
         errors.lastName = "Last Name can't be empty!";
+    } else if (!regex.test(input.lastName.trim())) {
+        errors.lastName = "Invalid format!";
     }
 
     if (input.departureAirportId === input.arrivalAirportId) {
